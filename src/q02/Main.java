ef7711bb4acea580b3fa6007bf8d818389c474a1
pran-jul <tiwaripranjul75@gmail.com>
2026-04-07 22:45:02 +0530

    Update Main.java

--- src/q02/Main.java
+++ src/q02/Main.java
@@ -29,15 +29,4 @@ public class Main {
     }
 }
 
-        // TODO: Print Pascal's Triangle for n rows
-        //       Values on each row separated by single space
-        //       No leading or trailing spaces
-        //
-        // Input: 5
-        // Output:
-        // 1
-        // 1 1
-        // 1 2 1
-        // 1 3 3 1
-        // 1 4 6 4 1
-
+        
