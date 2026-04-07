80ce9adf6a06c152395e72d07e6cd38c58274d6f
pran-jul <tiwaripranjul75@gmail.com>
2026-04-07 11:17:08 +0530

    hollow diamond solution
    Implemented hollow diamond pattern using nested loops.
    Takes input n and prints the required pattern.

--- src/q01/Main.java
+++ src/q01/Main.java
@@ -4,6 +4,48 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
+        import java.util.*;
+        // Upper part
+        for (int i = 1; i <= n; i++) {
+
+            // Print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // Print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
 
         // TODO: Print a hollow diamond pattern of half-height n using '*'
         //
@@ -20,6 +62,3 @@ public class Main {
         //  *   *
         //   * *
         //    *
-
-    }
-}
